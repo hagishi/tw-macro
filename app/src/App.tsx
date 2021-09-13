bc65b0a6978c75f2c2675ef0e3abaea39897652d
hagishi <fujiyama@kakumei.jp>
2021-09-13 19:29:38 +0900

    init

---
+++ app/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from '@hagishi/react-ui/src/components';
+
+const App = () => (
+  <div>
+    <h1>hello</h1>
+    <Button>aaaaa</Button>
+  </div>
+)
+
+export default App
